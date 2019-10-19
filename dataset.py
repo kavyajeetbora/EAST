8d7bb43d14f889ab363b045795e8f7b8b36cec76
Kavyajeet Bora <38955297+kavyajeetbora@users.noreply.github.com>
2019-10-19 19:13:49 +0530

    input format changed to x1,x2....y4

--- dataset.py
+++ dataset.py
@@ -361,7 +361,8 @@ def extract_vertices(lines):
 	labels = []
 	vertices = []
 	for line in lines:
-		vertices.append(list(map(int,line.rstrip('\n').lstrip('\ufeff').split(',')[:8])))
+		x1,x2,x3,x4,y1,y2,y3,y4 = list(map(int,line.rstrip('\n').lstrip('\ufeff').split(',')[:8]))
+		vertices.append([x1,y1,x2,y2,x3,y3,x4,y4])
 		label = 0 if '##::' in line else 1
 		labels.append(label)
 	return np.array(vertices), np.array(labels)
